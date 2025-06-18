bb0d7bb268db824dab661d2bc10b5e0a8b760cde
Aman Korabu <amankorabu24@gmail.com>
2025-06-18 15:55:15 +0530

    css changes

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -21,13 +21,14 @@ const Home = () => {
   
       <div class="card">
         <div className='main'>
+        <p>I'am</p>
         <div class="loader">
-        
           <div class="words">
-            <span class="word">Developer</span>
             <span class="word">Designer</span>
-            <span class="word">Coder</span>
-            <span class="word">Problem solver</span>
+            <span class="word">Designer</span>
+            <span class="word">Developer</span>
+            <span class="word">Web Enthusiast</span>
+            <span class="word">Problem solver Coder</span>
             <span class="word">React Developer</span>
           </div>
         </div>
