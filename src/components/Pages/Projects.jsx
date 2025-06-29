5b9ee11fb76f791d14c34f95073c860bbf55a0d1
Aman Korabu <amankorabu24@gmail.com>
2025-06-30 00:28:19 +0530

    some major changes in css and added components of project

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import { GoProjectSymlink } from "react-icons/go";
+import mass from '../images/certificates/mass.png'
+import con from '../images/certificates/contry.jpg'
+import por from '../images/certificates/portfolio.png'
 
+import { NavLink } from 'react-router-dom';
 function Projects() {
   const projectList = [
-    { title: 'Weather App', description: 'Shows weather based on location.' },
-    { title: 'Portfolio', description: 'My personal portfolio built with React.' },
-    { title: 'Todo App', description: 'A simple todo list with CRUD features.' }
+    {
+      title: 'Mass Coating Powder Coating & Coloring',
+      description: "MASS COATING is a partnership firm formed since 15th Feb 2016 .The company's registered office is at Bhosari,Pune.",
+      image: mass,
+      url: "https://bca-final-project-2.onrender.com "
+    },
+    {
+      title: 'Country Data',
+      description: 'All country data in the world with some Information related to country',
+      image: con,
+      url: "https://countrydataweb.netlify.app"
+    },
+    {
+      title: 'My Portfolio',
+      description: 'My portfolio which showcase my skills and my self..',
+      image: por,
+      url: "https://heycmyportfolio.netlify.app/"
+    }
   ];
+
 
   return (
     <div className="projects">
-
       <div className="skills-head" style={{
         display: 'flex', justifyContent: 'center', gap: '10px', alignItems: 'center',
       }}>
@@ -20,13 +39,36 @@ function Projects() {
         <br />
 
       </div>
-      <div className="project-grid">
+      <div className='prs' >
         {projectList.map((proj, index) => (
-          <div className="project-card" key={index}>
-            <h3>{proj.title}</h3>
-            <p>{proj.description}</p>
-          </div>
+          <NavLink to={proj.url} target="_blank">
+            <div
+              className="book"
+              key={index}>
+              <h3>{proj.title}</h3>
+              <p>{proj.description}</p>
+              <button type="button" class="btn3">
+                <strong>Click to Visit</strong>
+                <div id="container-stars">
+                  <div id="stars"></div>
+                </div>
+
+                <div id="glow">
+                  <div class="circle"></div>
+                  <div class="circle"></div>
+                </div>
+              </button>
+              <div className="cover" style={{
+                backgroundImage: `url(${proj.image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                // color: 'white',
+              }} >
+              </div>
+            </div>
+          </NavLink>
         ))}
+
       </div>
     </div>
   );
