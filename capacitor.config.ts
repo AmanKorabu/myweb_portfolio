f23ce1b4bc32c09676041ca5fbc1b05355726081
Aman Korabu <amankorabu24@gmail.com>
2025-09-15 12:00:05 +0530

    all extra files are deleted

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.myreact.app',  // unique app id
-  appName: 'portfolio',      // your app name
-  webDir: 'dist'            // React build folder
-};
-
-export default config;
