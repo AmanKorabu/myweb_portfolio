6d6d1c118e6033e04401ee1b7c9c855bb5f4ce55
Aman Korabu <amankorabu24@gmail.com>
2025-11-20 12:03:29 +0530

    changed the resume

--- src/components/Pages/Resume.jsx
+++ src/components/Pages/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from '../../assets/rezume.pdf (10).pdf'
+import resume from '../../assets/resume.pdf'
 function Resume() {
   return (
 <a href={resume} target="_blank"><button class="shadow__btn"  >
